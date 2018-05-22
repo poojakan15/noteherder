a8a61fc5e3bed09de4a128ae49afb59782df8afe
Pooja Kanuri <pk.musikluvr@gmail.com>
2018-05-22 11:26:19 -0400

    Added: styled main using css in Main.js

--- src/Main.js
+++ src/Main.js
@@ -7,12 +7,19 @@ import Sidebar from './Sidebar';
 
 const Main = () => {
     return (
-        <div className="Main">
+        <div className="Main" style={style}>
             <Sidebar />
             <NoteList />
             <NoteForm />
         </div>
     )
+}
+
+const style = {
+    // color: 'red', 
+    display: 'flex',
+    height: '100vh',
+    alignItems: 'stretch'
 }
 
 export default Main
