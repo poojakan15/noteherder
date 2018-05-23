d1f3183dd20e63d212419c293979a74830cc2546
Pooja Kanuri <pk.musikluvr@gmail.com>
2018-05-23 11:04:32 -0400

    Updated: loaded current note into noteForm

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -3,26 +3,30 @@ import React from 'react'
 import './NoteForm.css'
 
 const NoteForm = (props) => {
-    return (
-      <div className="NoteForm">
-        <div className="form-actions">
-          <button type="button">
-            <i className="far fa-trash-alt"></i>
-          </button>
-        </div>
-        <form>
-          <p>
-            <input
-              type="text"
-              name="title"
-              placeholder="Title your note"
-            />
-          </p>
-  
-          <textarea name="body"></textarea>
-        </form>
+  return (
+    <div className="NoteForm">
+      <div className="form-actions">
+        <button type="button">
+          <i className="far fa-trash-alt"></i>
+        </button>
       </div>
-    )
-  }
+      <form>
+        <p>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title your note"
+            value={props.currentNote.title}
+          />
+        </p>
+
+        <textarea
+          name="body"
+          value={props.currentNote.body}
+        ></textarea>
+      </form>
+    </div>
+  )
+}
 
 export default NoteForm
