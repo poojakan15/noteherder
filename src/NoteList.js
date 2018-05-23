73989aeecf6f36d8c4dfdbee9347d54b5a669277
Pooja Kanuri <pk.musikluvr@gmail.com>
2018-05-23 09:51:09 -0400

    Added: style to noteList

--- src/NoteList.js
+++ src/NoteList.js
@@ -5,7 +5,23 @@ import './NoteList.css'
 const NoteList = () => {
     return (
         <div className="NoteList">
-            <h1>Notes</h1>
+          <h3>Notes</h3>
+          <ul id="notes">
+            <a class="active">
+              <li>
+                <div class="note">
+                  <div class="note-title">
+                    Kohlrabi welsh
+                  </div>
+                  <div class="note-body">
+                    <p>
+                      Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.
+                    </p>
+                  </div>
+                </div>
+              </li>
+            </a>
+            </ul>
         </div>
     )
 }
