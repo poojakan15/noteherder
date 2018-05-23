f08b1030dc43d305225d52da09dea63045a1111b
Pooja Kanuri <pk.musikluvr@gmail.com>
2018-05-23 09:43:30 -0400

    Added: styling to sidebar.js, new version of fontawesome

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,67 +1,113 @@
 import React from 'react'
+import { StyleSheet, css } from 'aphrodite'
 
 import quill from './quill.svg'
 import newIcon from './new.png'
 import newHover from './new-hover.png'
-import './Sidebar.css'
+// import './Sidebar.css'
 
-const Sidebar = () => {
-    return (
-        <div 
-            className="Sidebar" 
-            // style={styles.sidebar}
-        >
-            <div 
-                className="logo" 
-                // style={styles.logo}
-            >
-                <img 
-                    src={quill} 
-                    alt="Noteherder" 
-                    // style={styles.logoImg}
-                />
-            </div>
-
-            <a href="/notes">
-                <img 
-                    src={newHover} 
-                    alt="New Note" 
-                />
-                <img 
-                    src={newIcon} 
-                    alt="New Note" 
-                />
-            </a>
-
-            <div className="SignOut">
-                <button>
-                    <i className="fa fa-sign-out"></i>
-                </button>
-            </div>
-
-        </div>
-    )
-}
-
-// const styles = {
-//     sidebar: {
-//         width: '6rem',
-//         backgroundColor: '#f3f3f3',
-//         padding: '0.5rem 0',
-//         display: 'flex',
-//         flexDirection: 'column',
-//         alignItems: 'center',
-//     },
-//     logo: {
-//         fontFamily: 'Fauna One',
-//         color: '#666',
-//         fontSize: '3rem',
-//     },
-//     logoImg: {
-//         width: '3rem',
-//         paddingLeft: '0.4rem',
-
+// class Sidebar extends React.Component {
+//     state = {
+//         newIconHovered: false,
 //     }
+
+//     handleMouseEnter() {
+//         this.setState( {
+//             newIconHovered: true
+//         } ) 
+//     }
+
+//     handleMouseLeave() {
+//         this.setState( {
+//             newIconHovered: false
+//         } ) 
+//     }
+
+//     render() {
+    const Sidebar = () => {
+        return (
+          <nav className={css(styles.sidebar)}>
+            <div className={css(styles.logo)}>
+              <img
+                src={quill}
+                alt="Noteherder"
+                className={css(styles.logoImg)}
+              />
+            </div>
+            <a
+              href="/notes"
+              className={css(styles.newNote)}
+            >
+              <img
+                src={newHover}
+                alt="New note"
+                className={css(styles.newNoteImg)}
+              />
+              <img
+                src={newIcon}
+                alt="New note"
+                className={css(styles.newNoteImg, styles.newNoteImgHover)}
+              />
+            </a>
+            <div className={css(styles.signOut)}>
+              <button className={css(styles.button)}>
+                <i className={`fa fa-sign-out ${css(styles.buttonIcon)}`}></i>
+              </button>
+            </div>
+          </nav>
+        )
+      }
 // }
+
+const styles = StyleSheet.create({
+    sidebar: {
+      width: '6rem',
+      backgroundColor: '#f3f3f3',
+      padding: '0.5rem 0',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    logo: {
+      fontFamily: '"Fauna One"',
+      color: '#666',
+      fontSize: '3rem',
+    },
+    logoImg: {
+      width: '3rem',
+      paddingLeft: '0.4rem',
+    },
+    newNote: {
+      marginTop: '2rem',
+      position: 'relative',
+      width: '4rem',
+    },
+    newNoteImg: {
+      position: 'absolute',
+      left: '0',
+      width: '100%',
+      transition: 'opacity 0.25s ease-in-out',
+    },
+    newNoteImgHover: {
+      ':hover': {
+        opacity: 0,
+      },
+    },
+    signOut: {
+      position: 'absolute',
+      bottom: '1rem',
+    },
+    button: {
+      backgroundColor: 'transparent',
+      border: '0',
+      color: '#008bf8',
+      cursor: 'pointer',
+      outline: 'none',
+    },
+    buttonIcon: {
+      fontSize: '2rem',
+    },
+  })
+
 
 export default Sidebar
