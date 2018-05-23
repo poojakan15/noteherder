6057db0db8bf7410ee5856ab74273f52faa78b0a
Pooja Kanuri <pk.musikluvr@gmail.com>
2018-05-23 10:01:33 -0400

    Updated: noteForm.js

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -4,13 +4,25 @@ import './NoteForm.css'
 
 const NoteForm = () => {
     return (
-        <div className="NoteForm">
-            <button type="button">
-                <i className="fa fa-trash-o"></i>
-            </button>
-            <input type="text" placeholder="Title.." />
+      <div className="NoteForm">
+        <div className="form-actions">
+          <button type="button">
+            <i className="far fa-trash-alt"></i>
+          </button>
         </div>
+        <form>
+          <p>
+            <input
+              type="text"
+              name="title"
+              placeholder="Title your note"
+            />
+          </p>
+  
+          <textarea name="body"></textarea>
+        </form>
+      </div>
     )
-}
+  }
 
 export default NoteForm
