7ca36cc4235c1bacbc97d49d1fe9061eeb8ef819
Pooja Kanuri <pk.musikluvr@gmail.com>
2018-05-24 18:20:06 -0400

    Updated: using linkWithPopup instead of signInWithPopup

--- src/SignIn.js
+++ src/SignIn.js
@@ -5,7 +5,7 @@ import { auth ,githubProvider, googleProvider } from './base'
 
 const SignIn = () => {
     const authenticate = (provider) =>{
-        auth.signInWithPopup(provider)
+        auth.linkWithPopup(provider)
     }
     return (
         <div className="SignIn">
