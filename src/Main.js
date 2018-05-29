83b55a4453ada3d6c751300d7c5c35a8f2b0313f
Pooja Kanuri <pk.musikluvr@gmail.com>
2018-05-29 09:33:31 -0400

    Updated: chanegd endpoint where notes are synced

--- src/Main.js
+++ src/Main.js
@@ -22,7 +22,7 @@ class Main extends React.Component {
 //   }
 
   componentWillMount() {
-      base.syncState(`${this.props.uid}`, {
+      base.syncState(`notes/${this.props.uid}`, {
           context: this,
           state: 'notes',
           asArray: true,
