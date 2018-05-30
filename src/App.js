4934edfc5c8f4afa45e901b22267b827001e9fb4
Pooja Kanuri <pk.musikluvr@gmail.com>
2018-05-30 09:35:34 -0400

    Added: routes for sign in and main

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
 
 import './App.css'
 import Main from './Main'
@@ -42,12 +43,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {
+      <Switch>
+        <Route path="/sign-in" component={SignIn} />
+        <Route path="/notes" render={() => <Main  signOut={this.signOut} uid={this.state.uid}/> }/>
+      </Switch>
+        {/* {
           this.signedIn() 
             ? <Main  signOut={this.signOut} uid={this.state.uid}/> 
             : <SignIn handleAuth={this.handleAuth}/>
         }
-        {/* <Main /> */}
+        <Main /> */}
        
       </div>
     )
